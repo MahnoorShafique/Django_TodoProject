3b70c5a77b94e5bd8a589856e89e51753788bc64
Mahnoor Shafique <mahnoorshafique623@gmail.com>
2022-10-14 16:31:07 +0500

    finally

--- todo_app/views.py
+++ todo_app/views.py
@@ -15,8 +15,6 @@ class TodoView(View):
     def post(self, request):
         data_dict=json.loads(request.body)
         date_data = data_dict["due_date"]
-        keys=[*data_dict]
-        # if set(keys) in {"task_name","description","due_date"}:
         if (isinstance(date_data, str)):
             converted_date = parser.parse(date_data)
             data_str = str(converted_date)
@@ -26,11 +24,6 @@ class TodoView(View):
         try:
 
             task_obj = Todo.objects.create(**data_dict)
-    
-            # task_obj=Todo(task_name=data_dict['task_name'],due_date=data_dict['due_date'],
-            # description=data_dict['description'],status=data_dict.get('status'))
-
-            # task_obj.save()
             return_data={
                 "msg":f'Data added against {task_obj.task_id}',
                 "status":True
@@ -42,14 +35,7 @@ class TodoView(View):
                 'status':False
             }
             return JsonResponse(response)
-        # else:
-        #      response={
-        #             "msg":"Params missing",
-        #             'status':False
-        #         }
-        #      return JsonResponse(response)
-
-
+    
         
         
     def get(self,request):
